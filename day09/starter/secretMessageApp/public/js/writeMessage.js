d944fea71bb8d2f3570e2c32d92cb45010ba0932
Khloe Ramdhan <khloe.ramdhan@outlook.com>
2021-07-22 22:32:51 +0000

    day 9 activity (completed with Sia)

--- day09/starter/secretMessageApp/public/js/writeMessage.js
+++ day09/starter/secretMessageApp/public/js/writeMessage.js
@@ -1,3 +1,16 @@
+function numeric(inputtxt)
+{
+    var nonnumbers = /^[a-zA-Z-,_!@#$%^&*().:;"'/?|]+$/;
+    if((inputtxt.match(nonnumbers)))
+    {
+        return true;
+    }
+    else
+    { 
+        // If returns false means there is a number
+        return false; 
+    }
+}
 const submitMessage = () => {
     console.log("Submitting message...")
     const passcodeInput = document.querySelector("#passcode");
@@ -5,10 +18,24 @@ const submitMessage = () => {
     const messageValue = messageInput.value;
     const passcodeValue = passcodeInput.value;
 
-    firebase.database().ref().push({
-        message: messageValue,
-        passcode: passcodeValue
-    });
+    const messageLength = messageValue.length;
+    const messageLimit = 25;
+
+    if (messageLength <= 25) {
+        if(numeric(passcodeValue) === false) {
+            firebase.database().ref().push({
+            message: messageValue,
+            passcode: passcodeValue
+            });
+        }
+        else {
+            alert('Error! Passcode must contain a number and capital letter')
+        }
+    }
+    else {
+        alert(`Error! Message is over the ${messageLimit} character limit.`)
+    }
+        
 
     passcodeInput.value = ''
     messageInput.value = ''
